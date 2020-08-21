dc6cb42035bc983ecb4f3cec1e7816a83e80f799
Jakub Lála <jakublala@gmail.com>
2020-08-21 10:57:26 +0200

    Update create_biases_from_pmfs.py

--- scripts/simutils/create_biases_from_pmfs.py
+++ scripts/simutils/create_biases_from_pmfs.py
@@ -10,7 +10,7 @@ from origamipy import read_windows_file
 def main():
     filebase = 'inps/snodin-staple_temp-344_run-0_rep-0'
     wins_filename = 'inps/snodin_344_staple.windows'
-    wins = origamipy.read_windows_file(wins_filename)
+    wins = read_windows_file(wins_filename)
     pmfs_filename = 'outs/snodin-long_temp-344_run-1_rep-0_pmfs.sds'
     pmf_array = np.loadtxt(pmfs_filename, skiprows=1)
     pmfs = {(i[0], i[1]): i[2] for i in pmf_array}
