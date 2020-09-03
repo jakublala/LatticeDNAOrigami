62a724211765ee3e2bc7b338e2eee694b5628eb0
Jakub Lála <jakublala@gmail.com>
2020-09-03 13:29:48 +0200

    update

--- scripts/sequtils/random_sequence.py
+++ scripts/sequtils/random_sequence.py
@@ -1,7 +1,8 @@
 import numpy as np
-import sys
+#import sys
 
-length = int(sys.argv[1])
+sequenceLength = input('Enter sequence length:')
+length = int(sequenceLength) #int(sys.argv[1])
 
 sequence = ''.join(np.random.choice(['A', 'C', 'G', 'T'], size=length).tolist())
 print(sequence)
