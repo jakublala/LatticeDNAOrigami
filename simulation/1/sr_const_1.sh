39afca851268d8b62aa05956ea69d5787178e30f
Jakub Lála <jakublala@gmail.com>
2020-09-08 11:22:54 +0200

    sr

---
+++ simulation/1/sr_const_1.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Name of job
+#SBATCH -J sr_const_1
+
+# Walltime limit (hours:mins:secs)
+#SBATCH -t 5:00:00
+
+# Nodes and procs
+#SBATCH -N 1
+#SBATCH -n 1
+
+
+# Standard error and out files
+#SBATCH -o outs_1/sr_const_$ID.o
+#SBATCH -e outs_1/sr_const_$ID.e
+
+module unload gcc
+module load gcc/6.2.0
+
+echo "Starting job $SLURM_JOB_ID"
+
+export LD_LIBRARY_PATH=~/lib:$LD_LIBRARY_PATH
+export PATH=~/bin/$PATH
+mkdir -p outs
+
+# Main job
+~/LatticeDNAOrigami/bin/latticeDNAOrigami -i sr_const_1.inp > outs_$ID/sr_const_$ID.out
+
+echo
+echo "Job finished. SLURM details are:"
+echo
+qstat -f ${SLURM_JOB_ID}
+echo
+echo Finished at `date`
