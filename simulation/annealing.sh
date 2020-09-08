cbb6ffecb095ff49bcc01980ba639565d7b5597e
Jakub Lála <jakublala@gmail.com>
2020-09-08 12:54:42 +0200

    annealing

---
+++ simulation/annealing.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Name of job
+#SBATCH -J annealing
+
+# Walltime limit (hours:mins:secs)
+#SBATCH -t 5:00:00
+
+# Nodes and procs
+#SBATCH -N 1
+#SBATCH -n 1
+
+# Standard error and out files
+#SBATCH -o outs_annealing/test_const.o
+#SBATCH -e outs_annealing/test_const.e
+
+module unload gcc
+module load gcc/6.2.0
+
+echo "Starting job $SLURM_JOB_ID"
+
+export LD_LIBRARY_PATH=~/lib:$LD_LIBRARY_PATH
+export PATH=~/bin/$PATH
+mkdir -p outs_annealing
+
+# Main job
+~/LatticeDNAOrigami/bin/latticeDNAOrigami -i annealing.inp > outs_annealing/annealing.out
+
+echo
+echo "Job finished. SLURM details are:"
+echo
+qstat -f ${SLURM_JOB_ID}
+echo
+echo Finished at `date`
