988db164a16761e5f8d2fe587fe905d1adc8421c
Jakub Lála <jakublala@gmail.com>
2020-09-08 11:02:50 +0200

    yes

--- simulation/sr_run_all.sh
+++ simulation/sr_run_all.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-sbatch 1/sr_const_1.sh
-sbatch 2/sr_const_2.sh
-sbatch 3/sr_const_3.sh
-sbatch 4/sr_const_4.sh
-sbatch 5/sr_const_5.sh
-sbatch 6/sr_const_6.sh
-sbatch 7/sr_const_7.sh
-sbatch 8/sr_const_8.sh
-sbatch 9/sr_const_9.sh
-sbatch 10/sr_const_10.sh
-sbatch 11/sr_const_11.sh
-sbatch 12/sr_const_12.sh
+sbatch slurms/sr_const_1.sh
+sbatch slurms/sr_const_2.sh
+sbatch slurms/sr_const_3.sh
+sbatch slurms/sr_const_4.sh
+sbatch slurms/sr_const_5.sh
+sbatch slurms/sr_const_6.sh
+sbatch slurms/sr_const_7.sh
+sbatch slurms/sr_const_8.sh
+sbatch slurms/sr_const_9.sh
+sbatch slurms/sr_const_10.sh
+sbatch slurms/sr_const_11.sh
+sbatch slurms/sr_const_12.sh
