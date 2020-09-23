d5e17b651055e81a1dd09a62d981086b78e8ebcb
Jakub Lála <jakublala@gmail.com>
2020-09-23 10:55:04 +0200

    cor

--- simulation/8_12/dr_const_8_12.sh
+++ simulation/8_12/dr_const_8_12.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Name of job
-#SBATCH -J dr_const_$ID
+#SBATCH -J dr_const_8_12
 
 # Walltime limit (hours:mins:secs)
 #SBATCH -t 24:00:00
@@ -12,8 +12,8 @@
 
 
 # Standard error and out files
-#SBATCH -o outs_$ID/dr_const_$ID.o
-#SBATCH -e outs_$ID/dr_const_$ID.e
+#SBATCH -o outs_8_12/dr_const_8_12.o
+#SBATCH -e outs_8_12/dr_const_8_12.e
 
 module unload gcc
 module load gcc/6.2.0
@@ -22,10 +22,10 @@ echo "Starting job $SLURM_JOB_ID"
 
 export LD_LIBRARY_PATH=~/lib:$LD_LIBRARY_PATH
 export PATH=~/bin/$PATH
-mkdir -p outs_$ID
+mkdir -p outs_8_12
 
 # Main job
-~/LatticeDNAOrigami/bin/latticeDNAOrigami -i dr_const_$ID.inp > outs_$ID/dr_const_$ID.out
+~/LatticeDNAOrigami/bin/latticeDNAOrigami -i dr_const_8_12.inp > outs_8_12/dr_const_8_12.out
 
 echo
 echo "Job finished. SLURM details are:"
